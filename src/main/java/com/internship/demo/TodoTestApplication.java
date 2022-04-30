16980a34440c089152ca311be92adece593c8ef5
Bojan Kelecevic <b.kelecevic97@gmail.com>
2022-04-30 11:33:54 +0200

    initialized commit

---
+++ src/main/java/com/internship/demo/TodoTestApplication.java
@@ -0,0 +1,13 @@
+package com.internship.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoTestApplication.class, args);
+	}
+
+}
